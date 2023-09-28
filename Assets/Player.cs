1185d7c9028e18f5f96e64b7e17bca550892b5e9
naoise collins <naoisecollins@gmail.com>
2023-09-28 10:30:57 +0100

    Add player script

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity = 2.4f;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            rigidbody.velocity = Vector2.up * velocity; 
+        }
+    }
+}
